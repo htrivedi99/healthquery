7d8914f29c02a995f6474a38da411756442f24ad
emmabernicerivera <erive011@ucr.edu>
2020-05-22 13:31:45 -0700

    Updated landing page UI

--- frontend/healthapp/src/LandingPage.js
+++ frontend/healthapp/src/LandingPage.js
@@ -2,62 +2,70 @@ import React, { Component } from 'react';
 
 const styles = {
     login: {
-        width: "30%",
-        cursor: "pointer",
-        marginRight: "0.25em",
-        marginTop: "0.5em",
-        padding: "0.938em",
-        border: "none",
+        width: '50%',
+        cursor: 'pointer',
+        marginRight: '0.25em',
+        marginTop: '0.5em',
+        padding: '0.938em',
+        border: 'none',
         borderRadius: 10,
-        backgroundColor: "#36356b",
-        color: "#fefefe",
+        backgroundColor: '#576CA8',
+        color: '#fefefe',
         fontSize: 20,
-        fontWeight: 600
-     },
-     signup: {
-        width: "30%",
-        cursor: "pointer",
-        marginRight: "0.25em",
-        marginTop: "0.5em",
-        padding: "0.938em",
-        border: "none",
+        fontWeight: 600,
+    },
+    signup: {
+        width: '50%',
+        cursor: 'pointer',
+        marginRight: '0.25em',
+        marginTop: '0.5em',
+        padding: '0.938em',
+        border: 'none',
         borderRadius: 10,
-        backgroundColor: "#ABABAB",
-        color: "#000",
+        backgroundColor: '#E5D4ED',
+        color: '#fefefe',
         fontSize: 20,
-        fontWeight: 600
-     }
-}
+        fontWeight: 600,
+    },
+};
 
 class LandingPage extends Component {
-    componentDidMount(){
-        document.body.style = 'background: #fff;';
+    componentDidMount() {
+        document.body.style = 'background: #36356b;';
     }
 
     goTologin = () => {
-        this.props.history.push("/login");
-    }
-    
-    goToSignup = () => {
-        this.props.history.push("/register");
-    }
+        this.props.history.push('/login');
+    };
 
-    render(){
-        return(
-            <div>
-                <div style={{display: "flex", alignItems: "center", textAlign: "center", justifyContent:"center"}}>
-                    <h1>Landing Page</h1>
-                </div>
-                <div style={{display: "flex", alignItems: "center", textAlign: "center", justifyContent:"center"}}>
-                    <button style={styles.login} onClick={this.goTologin}>
-                        Login
-                    </button>
-                    <button style={styles.signup} onClick={this.goToSignup}>
-                       Sign Up
-                    </button>
-                </div>
+    goToSignup = () => {
+        this.props.history.push('/register');
+    };
+
+    render() {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    marginRight: '20em',
+                    marginTop: '20em',
+                    marginLeft: '20em',
+                    marginBottom: '20em',
+                }}
+            >
+                <img src="./HQLogo.png" />
+                <img src="./HealthQueryTitleLogo.png" />
+
+                <button style={styles.login} onClick={this.goTologin}>
+                    Login
+                </button>
+                <button style={styles.signup} onClick={this.goToSignup}>
+                    Sign Up
+                </button>
             </div>
-            
         );
     }
 }
