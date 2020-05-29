69e163184e1edf660f71918ff2fedadee593671e
emmabernicerivera <erive011@ucr.edu>
2020-05-29 16:02:21 -0700

    Video call refractored. Working [fixme: css]

--- frontend/healthapp/src/App.js
+++ frontend/healthapp/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
-import Login from "./Login";
-import Register from "./Register";
-import LandingPage from "./LandingPage";
-import PatientPortal from "./PatientPortal";
+import Login from './Login';
+import Register from './Register';
+import LandingPage from './LandingPage';
+import PatientPortal from './PatientPortal';
+import VideoCall from './VideoCall';
 
 import './App.css';
 
 class App extends Component {
-  render(){
-    return(
-     <Router>
-       <Route exact path="/" component={LandingPage}/>
-       <Route exact path="/register" component={Register}/>
-       <Route exact path="/login" component={Login}/>
-       <Route exact path="/PatientPortal" component={PatientPortal}/>
-     </Router>
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/PatientPortal" component={PatientPortal} />
+        <Route exact path="/VideoCall" component={VideoCall} />
+      </Router>
     );
   }
 }
-
 
 export default App;
